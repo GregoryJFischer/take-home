21a68d94d36e0d2462b8d832a152c2f4c10f6e04
Gregory Fischer <67120586+GregoryJFischer@users.noreply.github.com>
2022-02-23 15:54:26 -0500

    Feat: add total price method

--- app/models/subscription_tea.rb
+++ app/models/subscription_tea.rb
@@ -1,4 +1,8 @@
 class SubscriptionTea < ApplicationRecord
   belongs_to :subscription
   belongs_to :tea
+
+  def self.total_price
+    sum("price * quantity")
+  end
 end
--- spec/models/subscription_tea_spec.rb
+++ spec/models/subscription_tea_spec.rb
@@ -12,4 +12,16 @@ describe SubscriptionTea, type: :model do
     expect(subscription_tea.price).to be_a Integer
     expect(subscription_tea.quantity).to be_a Integer
   end
+
+  describe 'class methods' do
+    describe '.total_price' do
+      it 'returns the total price of the subscription' do
+        create(:subscription_tea, price: 10, quantity: 1)
+        create(:subscription_tea, price: 20, quantity: 1)
+        create(:subscription_tea, price: 10, quantity: 2)
+
+        expect(SubscriptionTea.total_price).to eq 50
+      end
+    end
+  end
 end
